d8dd56b1c5c43a4cd4c7957087c9511ff4b8f0da
wdaherahmed <wdaherahmed@deloitte.com>
2024-08-30 13:22:59 +0200

    minor changes to docker config and start script

--- sod/sod-t3/start-dev.sh
+++ sod/sod-t3/start-dev.sh
@@ -4,10 +4,10 @@ while ! pg_isready -h db -p 5432 -U berry; do
   sleep 1
 done
 echo "Database is up - executing command"
-# npm i --save-dev prisma@latest                       │
-# npm i @prisma/client@latest
+
 npx prisma generate
 npx prisma migrate deploy
 echo "Migrations applied"
+
 npm run build
 npm run start
